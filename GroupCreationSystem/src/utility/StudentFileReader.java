d38b26e1dc78d255139807f0b42429f6e054fb6a
pmsc73 <philipmstc@gmail.com>
2014-11-18 19:14:44 -0330

    Forgot to add this file apparently :P StudentFileReader

---
+++ GroupCreationSystem/src/utility/StudentFileReader.java
@@ -0,0 +1,58 @@
+/**
+ * StudentFileReader.java
+ * This class is used to read in a file containing the list of
+ * student name and numbers, and produces a workable collection
+ * of Student objects.
+ * 
+ * @author pmsc73
+ * Updated 11/14/2014
+ */
+
+
+package utility;
+
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class StudentFileReader implements StudentReader {
+	public Collection<Student> getList(String path) {
+		/**
+		 * Only method that actually gets used in the class. Is full
+		 * class functionality.
+		 * @param path: path to file with list in it
+		 * @return studentList 
+		 */
+		Collection<Student> studentList = new ArrayList<Student>();
+		try { // needs to be in try block because Scanner throws FileNotFound
+			
+			// Scanner for entire file
+			Scanner fsc = new Scanner(new File(path)); 
+			while(fsc.hasNextLine()) {
+				// current line scanner
+				Scanner line = new Scanner(fsc.nextLine()); 
+				
+				// name and number separated by commas
+				line.useDelimiter(","); 
+				String name;
+				String number;
+				while(line.hasNext()) {
+					name = line.next();
+					if(line.hasNext()) {
+						number = line.next();
+						studentList.add(new Student(name,number));
+					}
+				}
+				line.close(); // eclipse suggest I close my scanners
+			}
+			fsc.close(); // eclipse suggests I close my scanners
+		}
+		catch(FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			System.exit(1);
+		}
+		return studentList;
+	}
+}
