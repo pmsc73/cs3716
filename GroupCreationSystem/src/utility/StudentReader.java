465fb235e132dba5cd96b4bf0a9bbddefc00dba0
Emily-Innes <emily.innes@live.com>
2014-11-18 17:37:37 -0330

    Apparently I don't know how to use Git. Tried to merge some stuff.

--- GroupCreationSystem/src/utility/StudentReader.java
+++ GroupCreationSystem/src/utility/StudentReader.java
@@ -1,8 +1,8 @@
 /**
- * StudentRead.java
- * This class is used to read in a file containing the list of
- * student name and numbers, and produces a workable collection
- * of Student objects.
+ * StudentReader.java
+ * Interface implemented by the StudentFileReader class
+ * This will be helpful if there is another implementation
+ * of reading from a list of students, such as database
  * 
  * @author pmsc73
  * Updated 11/14/2014
@@ -17,39 +17,9 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class StudentReader { 
-	public static Collection<Student> getList(File list) {
-		/**
-		 * Only method that actually gets used in the class. Is full
-		 * class functionality.
-		 * @param list : File containing student info
-		 * @return studentList 
-		 */
-		Collection<Student> studentList = new ArrayList<Student>();
-		try { // needs to be in try block because Scanner throws FileNotFound
-			
-			// Scanner for entire file
-			Scanner fsc = new Scanner(list); 
-			while(fsc.hasNextLine()) {
-				// current line scanner
-				Scanner line = new Scanner(fsc.nextLine()); 
-				
-				// name and number separated by commas
-				line.useDelimiter(","); 
-				String name;
-				String number;
-				while (!((name=line.next())==null) && !((number=line.next())==null)) {
-					// this ensures all students will have non-null fields
-					studentList.add(new Student(name,number));
-				}
-				line.close(); // eclipse suggest I close my scanners
-			}
-			fsc.close(); // eclipse suggests I close my scanners
-		}
-		catch(FileNotFoundException e) {
-			System.out.println(e.getMessage());
-			System.exit(1);
-		}
-		return studentList;
-	}
+public interface StudentReader {
+	public Collection<Student> getList(String path);
+	// For now the getList method takes some String arg,
+	// assuming the way we access a database, or whatever
+	// can be implemented via a string
 }
