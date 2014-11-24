8e21fce43fcf925cdcbc6bf72e2a0ae441eea7a5
Matt <mrh830@mun.ca>
2014-11-24 09:20:44 -0330

    All group creation is now inside CREATE button's listener.

--- GroupCreationSystem/src/gui/GroupProjectGUI.java
+++ GroupCreationSystem/src/gui/GroupProjectGUI.java
@@ -3,6 +3,7 @@
  */
 
 package gui;
+
 import group.*;
 
 import java.util.Collection;
@@ -33,25 +34,28 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 
 import java.awt.Font;
+
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 
-
 public class GroupProjectGUI extends JFrame {
 	private Controller controller;
 	private JPanel contentPane;
-	private JTextField txtCourseNumber;
-	private JTextField textField;
+	private JTextField txtCourse;
+	private JTextField txtSize;
+	private int groupSize;
+	private String courseName;
+	private boolean done;
 
 	/**
 	 * Launch the GUI page.
 	 */
-	public void start(){
-		//controller=this.controller; //intializes variables 
-		
+	public void start() {
+		// controller=this.controller; //intializes variables
+
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -68,241 +72,286 @@ public class GroupProjectGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public GroupProjectGUI(Controller controller) {
-		final Controller cont=controller;
+		final Controller cont = controller;
+
 		setTitle("Group Creation System");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		setBounds(100, 100, 555, 267);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel groupsPanel = new JPanel();
 		groupsPanel.setBounds(10, 85, 109, 131);
 		contentPane.add(groupsPanel);
 		groupsPanel.setLayout(null);
-		
-		cont.generateGroups(); // makes the groups in the controller
-		DefaultListModel<String> groupModel=new DefaultListModel<String>(); 
-		// groupModel is a list of the group names, effectively
-		
-		final Collection<Group> groups=cont.getGroups();
-		// list of all groups from controller
-		
-		for (Group group:groups){
-			groupModel.addElement(group.getName());
-			//the model gets each group name added
-		}
-		groupModel.addElement("Unassigned");
-		
-		final JList<String> groupList = new JList<String>(groupModel);
-		// groupList is a list who implements the model
-		groupList.setBounds(0, 0, 109, 230);
-		groupList.setBorder(null);
-		
-		JScrollPane scrlGroups = new JScrollPane();
-		scrlGroups.setViewportBorder(null);
-		scrlGroups.setViewportView(groupList);
-		scrlGroups.setBounds(0, 0, 109, 131);
-		groupsPanel.add(scrlGroups);
-		
-		JPanel groupMembersPanel = new JPanel();
-		groupMembersPanel.setBounds(172, 85, 252, 131);
-		contentPane.add(groupMembersPanel);
-		groupMembersPanel.setLayout(null);
-		
-		final DefaultListModel<String> lmModel = new DefaultListModel<String>();
-		// lmModel is like groupModel but for the GROUP MEMBER LIST
-		final JList<String> lstMembers = new JList<String>(lmModel);
-		// lstMembers is the JList who gets to hold lmModel
 
-		groupList.addListSelectionListener( new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent e) {
-				for(Group group:groups) {
-					group.printGroup(); // for debugging
-				}
-				int i = 0;
-				lmModel.clear(); //make sure lmModel is empty to begin
-				//handles getting the unassigned students
-				if(groupList.getSelectedValue()=="Unassigned"){
-					Collection<Student> unassignedCollection = cont.getUnassignedStudents();
-					for (Student stud:unassignedCollection){
-						lmModel.addElement(stud.getName());
-					}
-				}
-				else{
-					for(i=0;i<groups.size();i++){
-						//debugging print
-						System.out.println( ((ArrayList<Group>)groups).get(i).getName() + " : " + groupList.getSelectedValue());
-						
-						// tl;dr for this if: current selected group name == group(i) 
-						if ((((ArrayList<Group>)groups).get(i).getName()).equals(groupList.getSelectedValue())) {
-							System.out.println("success"); //prints our success
-							break;
-						}
-					}
-					ArrayList<Student> students = new ArrayList<Student>();
-					if (i<groups.size()) {
-						students = (ArrayList<Student>) ((ArrayList<Group>)groups).get(i).getGroupMembers();
-					}
-					for(Student student : students){ 
-						System.out.println(student.getName());
-						lmModel.addElement(student.getName());
-					}
-				}
-				
-			}
-		});
-		lstMembers.setBounds(0, 0, 252, 120);
-		
-		JScrollPane scrlMembers = new JScrollPane();
-		scrlMembers.setBounds(0, 0, 252, 130);
-		scrlMembers.setColumnHeaderView(lstMembers);
-		scrlMembers.setViewportView(lstMembers);
-		groupMembersPanel.add(scrlMembers);
-		
-		
-		
-		JButton moveButton = new JButton("MOVE");
-		
-		moveButton.setBounds(335, 156, 89, 23);
-		//listener for move button, onclick it opens a popup window
-		//for the group to move the selected student into.
-		//remember to allow moving student to unassigned.
-		DefaultListModel<String> popupGroupModel=new DefaultListModel<String>(); 
-		for (Group group:groups){
-			popupGroupModel.addElement(group.getName());
-			//the model gets each group name added
-		}
-		//HERE WE ARE WAHOO
-		popupGroupModel.addElement("Unassigned");
-		
-		final JList<String> popupGroupList = new JList<String>(popupGroupModel);
-		moveButton.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				final String selectedGroup = groupList.getSelectedValue();
-				final String selectedStudent= lstMembers.getSelectedValue();
-				if(selectedStudent == null){
-				}
-				else{
-					
-					final JDialog popup = new JDialog();
-					popup.getContentPane().setLayout(new FlowLayout());
-					popup.setSize(400,400);
-					
-					final JPanel popupGroups = new JPanel();
-					JLabel overhead = new JLabel("Move "+selectedStudent+" Where?");
-					overhead.setBounds(0, 0, 109, 14);
-					popup.getContentPane().add(overhead);
-					popup.setSize(300,300);
-					
-					JScrollPane groupsScroll = new JScrollPane();
-					groupsScroll.setViewportView(popupGroupList);
-					popupGroups.setBounds(0,20,200,300);
-					popupGroups.add(groupsScroll);
-					popup.getContentPane().add(popupGroups);
-					JButton moveIt =new JButton("MOVE");
-					//PUT IN MOVING TO THE ABYSS
-					moveIt.addActionListener(new ActionListener(){
-						public void actionPerformed(ActionEvent e){
-							//if from the unassigned list
-							String moveTo = popupGroupList.getSelectedValue(); 
-							Student student= cont.getStudentByName(selectedStudent);
-							if (selectedGroup =="Unassigned"){
-								if (moveTo=="Unassigned"){
-									//do nothing should change this
-								}
-								else{
-									Group toGroup = cont.getGroupByName(moveTo);
+		txtCourse = new JTextField();
+		txtCourse.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtCourse.setBounds(95, 11, 86, 20);
+		contentPane.add(txtCourse);
+		txtCourse.setColumns(10);
 
-									cont.addStudent(student, toGroup);
-								}
-							}
-							else{
-								Group fromGroup = cont.getGroupByName(selectedGroup);//HANDLE IF FROM GROUP IS UNASSIGNED
-								//check if group is full
-								if (moveTo=="Unassigned"){
-									cont.removeStudent(student, fromGroup);
-								}
-								else{
-									Group toGroup = cont.getGroupByName(moveTo);
-									
-									cont.removeStudent(student,fromGroup);
-									cont.addStudent(student, toGroup);
-									/* This was here for testing, had a problem tho, so ignoring for now
-									if(toGroup.isFull()) {
-										JDialog popup2 = new JDialog();
-										popup2.setSize(50,200);
-										popup2.add(new JLabel("WARNING: "+toGroup.getName()+" is full, moved anyways."));
-										popup2.setVisible(true);
-									}
-									*/
-								}
-							}
-							System.out.println("here");
-							groupList.setSelectedValue(moveTo,true);
-							popup.setVisible(false);
-						}
-					});
-					
-					moveIt.setBounds(100,100,120,120);
-					popup.getContentPane().add(moveIt);
-					popup.setVisible(true);
-				}
-			}
-		});
-		
-		contentPane.add(moveButton);
-		
-		JLabel groupsLabel = new JLabel("Group List:");
-		groupsLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		groupsLabel.setBounds(10, 60, 109, 14);
-		contentPane.add(groupsLabel);
-		
-		JLabel membersLabel = new JLabel("Group Members:");
-		membersLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		membersLabel.setBounds(176, 60, 117, 14);
-		contentPane.add(membersLabel);
-		
-		txtCourseNumber = new JTextField();
-		txtCourseNumber.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		txtCourseNumber.setBounds(95, 11, 86, 20);
-		contentPane.add(txtCourseNumber);
-		txtCourseNumber.setColumns(10);
-		
 		JLabel lblCourseName = new JLabel("Course name:");
 		lblCourseName.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblCourseName.setBounds(10, 14, 86, 14);
 		contentPane.add(lblCourseName);
-		
+
 		JLabel lblGroupSize = new JLabel("Group size:");
 		lblGroupSize.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblGroupSize.setBounds(202, 14, 71, 14);
 		contentPane.add(lblGroupSize);
-		
-		textField = new JTextField();
-		textField.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		textField.setBounds(272, 11, 86, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
-		
+
+		txtSize = new JTextField();
+		txtSize.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtSize.setBounds(272, 11, 86, 20);
+		contentPane.add(txtSize);
+		txtSize.setColumns(10);
+
 		JButton btnCreate = new JButton("CREATE");
+		btnCreate.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					groupSize = Integer.parseInt(txtSize.getText());
+				} catch (NumberFormatException NaN) {
+					txtSize.setText("Try again");
+					return;
+				}
+				courseName = txtCourse.getText();
+				if (courseName.equals("")) {
+					return;
+				}
+				cont.setCourseNumber(courseName);
+				cont.setGroupSize(groupSize);
+				cont.finalizeParameters();
+
+				cont.generateGroups(); // makes the groups in the controller
+				DefaultListModel<String> groupModel = new DefaultListModel<String>();
+				// groupModel is a list of the group names, effectively
+
+				final Collection<Group> groups = cont.getGroups();
+				// list of all groups from controller
+
+				for (Group group : groups) {
+					groupModel.addElement(group.getName());
+					// the model gets each group name added
+				}
+				groupModel.addElement("Unassigned");
+
+				final JList<String> groupList = new JList<String>(groupModel);
+				// groupList is a list who implements the model
+				groupList.setBounds(0, 0, 109, 230);
+				groupList.setBorder(null);
+
+				JScrollPane scrlGroups = new JScrollPane();
+				scrlGroups.setViewportBorder(null);
+				scrlGroups.setViewportView(groupList);
+				scrlGroups.setBounds(0, 0, 109, 131);
+				groupsPanel.add(scrlGroups);
+
+				JPanel groupMembersPanel = new JPanel();
+				groupMembersPanel.setBounds(172, 85, 252, 131);
+				contentPane.add(groupMembersPanel);
+				groupMembersPanel.setLayout(null);
+
+				final DefaultListModel<String> lmModel = new DefaultListModel<String>();
+				// lmModel is like groupModel but for the GROUP MEMBER LIST
+				final JList<String> lstMembers = new JList<String>(lmModel);
+				// lstMembers is the JList who gets to hold lmModel
+
+				groupList.addListSelectionListener(new ListSelectionListener() {
+					public void valueChanged(ListSelectionEvent e) {
+						for (Group group : groups) {
+							group.printGroup(); // for debugging
+						}
+						int i = 0;
+						lmModel.clear(); // make sure lmModel is empty to begin
+						// handles getting the unassigned students
+						if (groupList.getSelectedValue() == "Unassigned") {
+							Collection<Student> unassignedCollection = cont
+									.getUnassignedStudents();
+							for (Student stud : unassignedCollection) {
+								lmModel.addElement(stud.getName());
+							}
+						} else {
+							for (i = 0; i < groups.size(); i++) {
+								// debugging print
+								System.out.println(((ArrayList<Group>) groups)
+										.get(i).getName()
+										+ " : "
+										+ groupList.getSelectedValue());
+
+								// tl;dr for this if: current selected group
+								// name ==
+								// group(i)
+								if ((((ArrayList<Group>) groups).get(i)
+										.getName()).equals(groupList
+										.getSelectedValue())) {
+									System.out.println("success"); // prints our
+																	// success
+									break;
+								}
+							}
+							ArrayList<Student> students = new ArrayList<Student>();
+							if (i < groups.size()) {
+								students = (ArrayList<Student>) ((ArrayList<Group>) groups)
+										.get(i).getGroupMembers();
+							}
+							for (Student student : students) {
+								System.out.println(student.getName());
+								lmModel.addElement(student.getName());
+							}
+						}
+
+					}
+				});
+				lstMembers.setBounds(0, 0, 252, 120);
+
+				JScrollPane scrlMembers = new JScrollPane();
+				scrlMembers.setBounds(0, 0, 252, 130);
+				scrlMembers.setColumnHeaderView(lstMembers);
+				scrlMembers.setViewportView(lstMembers);
+				groupMembersPanel.add(scrlMembers);
+
+				JButton moveButton = new JButton("MOVE");
+
+				moveButton.setBounds(335, 156, 89, 23);
+				// listener for move button, onclick it opens a popup window
+				// for the group to move the selected student into.
+				// remember to allow moving student to unassigned.
+				DefaultListModel<String> popupGroupModel = new DefaultListModel<String>();
+				for (Group group : groups) {
+					popupGroupModel.addElement(group.getName());
+					// the model gets each group name added
+				}
+				// HERE WE ARE WAHOO
+				popupGroupModel.addElement("Unassigned");
+
+				final JList<String> popupGroupList = new JList<String>(
+						popupGroupModel);
+				moveButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						final String selectedGroup = groupList
+								.getSelectedValue();
+						final String selectedStudent = lstMembers
+								.getSelectedValue();
+						if (selectedStudent == null) {
+						} else {
+
+							final JDialog popup = new JDialog();
+							popup.getContentPane().setLayout(new FlowLayout());
+							popup.setSize(400, 400);
+
+							final JPanel popupGroups = new JPanel();
+							JLabel overhead = new JLabel("Move "
+									+ selectedStudent + " Where?");
+							overhead.setBounds(0, 0, 109, 14);
+							popup.getContentPane().add(overhead);
+							popup.setSize(300, 300);
+
+							JScrollPane groupsScroll = new JScrollPane();
+							groupsScroll.setViewportView(popupGroupList);
+							popupGroups.setBounds(0, 20, 200, 300);
+							popupGroups.add(groupsScroll);
+							popup.getContentPane().add(popupGroups);
+							JButton moveIt = new JButton("MOVE");
+							// PUT IN MOVING TO THE ABYSS
+							moveIt.addActionListener(new ActionListener() {
+								public void actionPerformed(ActionEvent e) {
+									// if from the unassigned list
+									String moveTo = popupGroupList
+											.getSelectedValue();
+									Student student = cont
+											.getStudentByName(selectedStudent);
+									if (selectedGroup == "Unassigned") {
+										if (moveTo == "Unassigned") {
+											// do nothing should change this
+										} else {
+											Group toGroup = cont
+													.getGroupByName(moveTo);
+
+											cont.addStudent(student, toGroup);
+										}
+									} else {
+										Group fromGroup = cont
+												.getGroupByName(selectedGroup);// HANDLE
+																				// IF
+																				// FROM
+																				// GROUP
+																				// IS
+																				// UNASSIGNED
+										// check if group is full
+										if (moveTo == "Unassigned") {
+											cont.removeStudent(student,
+													fromGroup);
+										} else {
+											Group toGroup = cont
+													.getGroupByName(moveTo);
+
+											cont.removeStudent(student,
+													fromGroup);
+											cont.addStudent(student, toGroup);
+											/*
+											 * This was here for testing, had a
+											 * problem tho, so ignoring for now
+											 * if(toGroup.isFull()) { JDialog
+											 * popup2 = new JDialog();
+											 * popup2.setSize(50,200);
+											 * popup2.add(new
+											 * JLabel("WARNING: "+toGroup.
+											 * getName
+											 * ()+" is full, moved anyways."));
+											 * popup2.setVisible(true); }
+											 */
+										}
+									}
+									System.out.println("here");
+									groupList.setSelectedValue(moveTo, true);
+									popup.setVisible(false);
+								}
+							});
+
+							moveIt.setBounds(100, 100, 120, 120);
+							popup.getContentPane().add(moveIt);
+							popup.setVisible(true);
+						}
+					}
+				});
+
+				contentPane.add(moveButton);
+
+				JLabel groupsLabel = new JLabel("Group List:");
+				groupsLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+				groupsLabel.setBounds(10, 60, 109, 14);
+				contentPane.add(groupsLabel);
+
+				JLabel membersLabel = new JLabel("Group Members:");
+				membersLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+				membersLabel.setBounds(176, 60, 117, 14);
+				contentPane.add(membersLabel);
+
+				JButton btnMove = new JButton("MOVE");
+				btnMove.setFont(new Font("Tahoma", Font.PLAIN, 12));
+				btnMove.setBounds(442, 84, 89, 23);
+				contentPane.add(btnMove);
+
+				JComboBox comboBox = new JComboBox();
+				comboBox.setFont(new Font("Tahoma", Font.PLAIN, 12));
+				comboBox.setModel(new DefaultComboBoxModel(new String[] {
+						"Group 1", "Group 2", "Group 3", "Group 4",
+						"One Fiech", "Two Fiech", "Red Fiech", "Blue Fiech" }));
+				comboBox.setBounds(442, 118, 89, 23);
+				contentPane.add(comboBox);
+
+			}
+
+		});
 		btnCreate.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		btnCreate.setBounds(398, 10, 89, 23);
 		contentPane.add(btnCreate);
-		
-		JButton btnMove = new JButton("MOVE");
-		btnMove.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnMove.setBounds(442, 84, 89, 23);
-		contentPane.add(btnMove);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Group 1", "Group 2", "Group 3", "Group 4", "One Fiech", "Two Fiech", "Red Fiech", "Blue Fiech"}));
-		comboBox.setBounds(442, 118, 89, 23);
-		contentPane.add(comboBox);
-		
+
 	}
 }
-
