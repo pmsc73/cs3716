0ced7a0d2543b6165083b3df8e5be1b82f9477b5
Noah <Noah@134.153.40.238>
2014-11-24 11:24:03 -0330

    update dat, started working on popup preferences

---
+++ GroupCreationSystem/src/gui/PreferencePopup.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package gui;
+import javax.swing.JDialog;
+
+import group.*;
+import utility.*;
+
+/**
+ * @author Noah
+ *
+ */
+public class PreferencePopup {
+	private Controller controller;
+	public PreferencePopup(Controller controller){
+		JDialog popup = new JDialog();
+		
+	}
+}
